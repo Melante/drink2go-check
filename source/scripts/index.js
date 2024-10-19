73e7bf735eafe3daa8ec1bf26cef962e6e2b4d52
Мария Бадулина <volshica1988@mail.ru>
2024-10-19 16:46:05 +0500

    Верстка фильтра

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -14,3 +14,29 @@ burger.onclick = function() {
   burger.classList.toggle('navigation__toggle--close');
   nav.classList.toggle('navigation__list--open');
 };
+
+
+const rangeSlider = document.querySelector('.rang__slider');
+
+if(rangeSlider) {
+  noUiSlider.create(rangeSlider, {
+    start: [0, 900],
+    connect: true,
+    step: 1,
+    range: {
+      'min': [0],
+      'max': [1000]
+    },
+    cssPrefix: 'noui-'
+  });
+
+  const inputLeft = document.querySelector('.min-input');
+  const inputRight = document.querySelector('.max-input');
+  const inputs = [inputLeft, inputRight];
+
+  rangeSlider.noUiSlider.on('update', (values, handle) =>{
+    inputs[handle].value = Math.round(values[handle]);
+  });
+}
+
+
